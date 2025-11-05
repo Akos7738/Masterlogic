03e0b046eb49f44f727ff3a665c38f31b8a41b0c
Akos 7738 <czmorekakos@gmail.com>
2025-11-05 22:07:59 +0100

    functions

--- app.js
+++ app.js
@@ -2,6 +2,139 @@ function getRndNum(start, end) {
     return Math.floor(Math.random() * end + start);
 }
 
+function controlls(rowNumber) {
+    const pegs = document.querySelectorAll(`.row${rowNumber} .peg`);
+    pegs.forEach(peg => {
+        let temp = 0;
+        peg.addEventListener("click", () => {
+            if (temp === 8) {
+                temp = 1;
+            }
+            else {
+                temp++;
+            }
+            switch(temp) {
+                case 1:
+                    peg.className = "peg red";
+                    break;
+                case 2:
+                    peg.className = "peg yellow";
+                    break;
+                case 3:
+                    peg.className = "peg green";
+                    break;
+                case 4:
+                    peg.className = "peg blue";
+                    break;
+                case 5:
+                    peg.className = "peg purple";
+                    break;
+                case 6:
+                    peg.className = "peg pink";
+                    break;
+                case 7:
+                    peg.className = "peg orange";
+                    break;
+                case 8:
+                    peg.className = "peg cyan";
+                    break;
+                default:
+                    peg.className = "peg";
+            }
+        });
+        peg.addEventListener("contextmenu", event => {
+            event.preventDefault();
+            if (temp <= 1) {
+                temp = 8;
+            }
+            else {
+                temp--;
+            }
+                    switch(temp) {
+                case 1:
+                    peg.className = "peg red";
+                    break;
+                case 2:
+                    peg.className = "peg yellow";
+                    break;
+                case 3:
+                    peg.className = "peg green";
+                    break;
+                case 4:
+                    peg.className = "peg blue";
+                    break;
+                case 5:
+                    peg.className = "peg purple";
+                    break;
+                case 6:
+                    peg.className = "peg pink";
+                    break;
+                case 7:
+                    peg.className = "peg orange";
+                    break;
+                case 8:
+                    peg.className = "peg cyan";
+                    break;
+                default:
+                    peg.className = "peg";
+            }
+        });
+    });
+}
+
+function check(rowNumber) {
+    document.querySelector(`.row${rowNumber} .checkbtn`).addEventListener("click", () => {
+        let tempSum = 0;
+        let checkList = [];
+        let tempList = [...rndlist];
+        let blacks = 0;
+        let whites = 0;
+        const pegs = document.querySelectorAll(`.row${rowNumber} .peg`);
+        pegs.forEach(peg => {
+            tempSum += peg.classList.length;
+            checkList.push(peg.classList.toString());
+        });
+        if (tempSum === 8) {
+            for (let i = 0; i < checkList.length; i++) {
+                checkList[i] = checkList[i].split(" ")[1];
+            }
+            for (let i = 0; i < rndlist.length; i++) {
+                if(tempList[i] === checkList[i]) {
+                    blacks++;
+                    tempList[i] = null;
+                    checkList[i] = null;
+                    console.log(checkList);
+                }
+            }
+            for (let i = 0; i < checkList.length; i++) {
+                if(!(tempList[i] === checkList[i]) && tempList.includes(checkList[i])) {
+                    whites++;
+                }
+            }
+            const dots = document.querySelectorAll(`.row${rowNumber} .dot`);
+            for (let i = 0; i < blacks; i++) {
+                dots[i].className = "dot black";
+            }
+            for (let i = 0; i < whites; i++) {
+                dots[i + blacks].className = "dot white";
+            }
+            if(blacks === 4) {
+                const top = document.querySelectorAll(".row0 .peg");
+                for (let i = 0; i < rndlist.length; i++) {
+                    top[i].innerText = "";
+                    top[i].classList.add(rndlist[i]);
+                }
+                document.querySelector(".container").classList.add("disabled");
+                alert("Gratulálok, Nyertél!");
+            }
+            document.querySelector(`.row${rowNumber}`).classList.add("disabled");
+        }
+        else{
+            alert("Mind a 4 helyre válasszon színt!");
+        }
+    });
+}
+
 document.querySelector(".controllsbtn").addEventListener("click", () => {
     alert("Az egér bal és jobb gombjával a nagy pöttyök színe változtatható.\nAz \"Ellenőrzés\" gombra kattintva láthatóvá válik az adott sor eredménye.")
 })
@@ -38,115 +171,5 @@ for (let i = 0; i < 4; i++) {
 
 console.log(rndlist);
 
-const pegs1 = document.querySelectorAll(".row1 .peg");
-
-pegs1.forEach(peg => {
-    let temp = 0;
-    peg.addEventListener("click", () => {
-        if (temp === 8) {
-            temp = 1;
-        }
-        else {
-            temp++;
-        }
-        switch(temp) {
-            case 1:
-                peg.className = "peg red";
-                break;
-            case 2:
-                peg.className = "peg yellow";
-                break;
-            case 3:
-                peg.className = "peg green";
-                break;
-            case 4:
-                peg.className = "peg blue";
-                break;
-            case 5:
-                peg.className = "peg purple";
-                break;
-            case 6:
-                peg.className = "peg pink";
-                break;
-            case 7:
-                peg.className = "peg orange";
-                break;
-            case 8:
-                peg.className = "peg cyan";
-                break;
-            default:
-                peg.className = "peg";
-        }
-    });
-    peg.addEventListener("contextmenu", event => {
-        event.preventDefault();
-        if (temp <= 1) {
-            temp = 8;
-        }
-        else {
-            temp--;
-        }
-                switch(temp) {
-            case 1:
-                peg.className = "peg red";
-                break;
-            case 2:
-                peg.className = "peg yellow";
-                break;
-            case 3:
-                peg.className = "peg green";
-                break;
-            case 4:
-                peg.className = "peg blue";
-                break;
-            case 5:
-                peg.className = "peg purple";
-                break;
-            case 6:
-                peg.className = "peg pink";
-                break;
-            case 7:
-                peg.className = "peg orange";
-                break;
-            case 8:
-                peg.className = "peg cyan";
-                break;
-            default:
-                peg.className = "peg";
-        }
-    });
-});
-
-document.querySelector(".row1 .checkbtn").addEventListener("click", () => {
-    let tempSum = 0;
-    let checkList1 = [];
-    let tempList = [...rndlist];
-    let blacks = 0;
-    let whites = 0;
-    pegs1.forEach(peg => {
-        tempSum += peg.classList.length;
-        checkList1.push(peg.classList.toString());
-    });
-    if (tempSum === 8) {
-        for (let i = 0; i < checkList1.length; i++) {
-            checkList1[i] = checkList1[i].split(" ")[1];
-        }
-        for (let i = 0; i < rndlist.length; i++) {
-            if(tempList[i] === checkList1[i]) {
-                blacks++;
-                tempList[i] = null;
-                checkList1[i] = null;
-            }
-            else if(tempList.includes(checkList1[i])) {
-                whites++;
-            }
-        }
-        const dots = document.querySelectorAll(".row1 .dot");
-        for (let i = 0; i < blacks; i++) {
-            dots[i].className = "dot black";
-        }
-        for (let i = 0; i < whites; i++) {
-            dots[i + blacks].className = "dot white";
-        }
-    }
-});
+controlls(1);
+check(1);
